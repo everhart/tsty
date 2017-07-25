d9406a662279654ed7b6b0611b32950424e463ca
everhart <rpe@rpe.io>
2017-07-25 08:30:38 -0400

    Added Rgb structure declaration.

--- tsty.hpp
+++ tsty.hpp
@@ -1,6 +1,3 @@
-#ifndef TSTY_HPP__
-#define TSTY_HPP__
-
 /*!
  *  @author Richard Everhart
  *  @date   July 2017
@@ -9,7 +6,30 @@
  *  functions
 !*/
 
+#ifndef TSTY_HPP__
+#define TSTY_HPP__
+
+#include <type_traits>
+
 namespace tsty {
+
+//! Rudimentary structure for storing RGB color values.
+struct Rgb {
+    unsigned char r = 0, g = 0, b = 0;
+
+    //! Constructor for initializing r, g, b values from a hexadecimal
+    explicit Rgb(unsigned int hex);
+
+    /*! 
+     *  Method for acquiring a hexadecimal representation of the rgb color 
+     *  data.
+     *
+     *  @return An unsigned integer that is the hexadecimal representation
+    !*/
+    unsigned int hex();
+};
+typedef Rgb rgb;
+
 }
 
 #endif
