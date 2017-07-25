7d23d859a2197bbc280db80a481aa435bef56eb1
everhart <rpe@rpe.io>
2017-07-25 08:57:35 -0400

    Added enum Emphasis for representing various ANSI SGR operations.

--- tsty.hpp
+++ tsty.hpp
@@ -30,6 +30,34 @@ struct Rgb {
 };
 typedef Rgb rgb;
 
+
+//! Enum containing flags representing various ANSI-Supported text emphasis
+enum Emphasis {
+    NORMAL          = 0x000001,
+    BOLD            = 0x000002,
+    FAINT           = 0x000004,
+    ITALIC          = 0x000008,
+    UNDERLINE       = 0x000010,
+    SLOW_BLINK      = 0x000020,
+    FAST_BLINK      = 0x000040,
+    NEGATIVE        = 0x000080,
+    CONCEAL         = 0x000100,
+    CROSS_OUT       = 0x000200,
+    FRAKTUR         = 0x000400,
+    NO_BOLD         = 0x000800,
+    NO_COLOR        = 0x001000,
+    NO_ITALIC       = 0x002000,
+    NO_UNDERLINE    = 0x004000,
+    POSITIVE        = 0x008000,
+    REVEAL          = 0x010000,
+    NO_CROSS_OUT    = 0x020000,
+    FRAME           = 0x040000,
+    ENCIRCLE        = 0x080000,
+    OVERLINE        = 0x100000,
+    NO_FRAME        = 0x200000,
+    NO_OVERLINE     = 0x400000
+};
+
 }
 
 #endif
