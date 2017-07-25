13ef09cf842c1da373ec5d8ba4f6c7c36bc8deba
everhart <rpe@rpe.io>
2017-07-25 08:00:16 -0400

    Added initial tsty.hpp file.

---
+++ tsty.hpp
@@ -0,0 +1,15 @@
+#ifndef TSTY_HPP__
+#define TSTY_HPP__
+
+/*!
+ *  @author Richard Everhart
+ *  @date   July 2017
+ *
+ *  Declarations and template definitions for all tsty library 
+ *  functions
+!*/
+
+namespace tsty {
+}
+
+#endif
